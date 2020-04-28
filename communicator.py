abdc09341a74328286a4746fc43355d309280d75
adirlou <adrien.vandenbroucque@gmail.com>
2020-04-28 13:48:24 +0200

    finished communicator

--- communicator.py
+++ communicator.py
@@ -1,3 +1,4 @@
+import networkx as nx
 import numpy as np
 
 class Communicator:
@@ -11,7 +12,7 @@ class Communicator:
         self.consensus_lr = consensus_lr
 
         # Validate the input parameters
-        self._validate_params()
+        self.__validate_params()
 
         # Set parameters that need to be computed (transition matrix)
         self.__set_params()
@@ -26,7 +27,7 @@ class Communicator:
         """Validate input parameters."""
 
         # Define the supported methods for communication
-        valid_methods = ['plain', 'choco', 'dcd_psgd', 'ecd_psgd']
+        valid_methods = ['plain', 'choco']
 
         # Check if method is valid
         if self.method not in valid_methods:
@@ -51,12 +52,20 @@ class Communicator:
             raise ValueError('Invalid consensus learning rate, must be in interval [0,1]')
 
         # Define the supported topologies
-        valid_topologies = ['disconnected', 'path', 'star', 'ring', 'complete', 'barbell']
+        valid_topologies = ['disconnected', 'path', 'star', 'ring', 'complete', 'barbell', 'torus']
 
         # Check if given String for topology is valid
         if isinstance(self.topology, str):
             if self.topology not in valid_topologies:
                 raise ValueError('Topology should be one of: ' + str(valid_topologies))
+
+            # If barbell topolgy, number of machines must be even
+            if self.topology == 'barbell' and self.n_machines % 2 != 0:
+                raise ValueError('For barbell topology, number of machines must be even')
+
+            # If torus topology, number of machines must be a perfect square
+            if self.topology == 'torus' and int(np.sqrt(self.n_machines)) ** 2 != self.n_machines:
+                raise ValueError('For torus topology, number of machines must be a perfect square')
 
         # Check if given transition matrix for topology is valid
         elif isinstance(self.topology, np.ndarray):
@@ -65,12 +74,12 @@ class Communicator:
             n_rows, n_cols = self.topology.shape
 
             # Check if the transition matrix is square
-            if self.topology.n_dim != 2 or n_rows != n_cols:
+            if self.topology.ndim != 2 or n_rows != n_cols:
                 raise ValueError('Invalid dimensions for transition matrix for topology')
 
             # Check if the transition matrix is symmetric
-            if not np.allclose(self.topology, self.topology.T):
-                raise ValueError('Invalid transition matrix, must be symmetric')
+            #if not np.allclose(self.topology, self.topology.T):
+            #    raise ValueError('Invalid transition matrix, must be symmetric')
 
             # Check if the transition matrix is aperiodic (must contain at least one self loop)
             if not np.diag(self.topology).sum() > 0:
@@ -89,50 +98,68 @@ class Communicator:
         else:
             raise ValueError('Invalid type for topology')
 
-    def __validate_transition_matrix(self):
+    def __create_valid_transition_matrix(self):
 
         # If the transition matrix is to be generated given a String
         if isinstance(self.topology, str):
-            # TODO implement networkx graph generation + add self-loops
+
+            # Get corresponding graph using NetworkX
+            if self.topology == 'disconnected':
+                graph = nx.generators.empty_graph(self.n_machines)
+            elif self.topology == 'path':
+                graph = nx.generators.path_graph(self.n_machines)
+            elif self.topology == 'star':
+                graph = nx.generators.star_graph(self.n_machines)
+            elif self.topology == 'ring':
+                graph = nx.generators.cycle_graph(self.n_machines)
+            elif self.topology == 'complete':
+                graph = nx.generators.complete_graph(self.n_machines)
+            elif self.topology == 'barbell':
+                graph = nx.generators.barbell_graph(self.n_machines // 2, 0)
+            elif self.topology == 'torus':
+                # Number of machines on "side" of torus
+                n_machines_on_side = int(np.sqrt(self.n_machines))
+                graph = nx.generators.lattice.grid_2d_graph(n_machines_on_side, n_machines_on_side, periodic=True)
+
+            # Get the adjacency matrix from the graph
+            adjacency_matrix = nx.adjacency_matrix(graph).toarray()
 
             # Add self-loops
-            adjacency_matrix = adjacency_matrix + np.eye(adjacency_matrix.shape[0])
-            return None
+            adjacency_matrix = adjacency_matrix + np.eye(self.n_machines)
 
-        # Else the transition matrix is directly given
+        # Else the transition matrix is directly given in parameter "topology"
         else:
-            transition_matrix = self.topology
-
-        # Get the number of
-        n_rows, _ = adjacency_matrix.shape
+            adjacency_matrix = self.topology
 
         # Make sure that the rows of the matrix are normalized
-        if not np.allclose(adjacency_matrix.sum(axis=1), np.ones(n_rows)):
+        if not np.allclose(adjacency_matrix.sum(axis=1), np.ones(self.n_machines)):
 
             # Normalize each row to be a valid Markov Chain
             row_sums = adjacency_matrix.sum(axis=1, keepdims=True)
             transition_matrix = adjacency_matrix / row_sums
 
+        # Else the adjacency matrix is already a valid transition matrix
+        else:
+            transition_matrix = adjacency_matrix
+
         return transition_matrix
 
     def communicate(self, weight_matrix):
+        """Perform the communication step of the decentralized SGD, given the weight of
+        all the machines as a matrix"""
+
+        # Choose the appropriate method
         if self.method == 'plain':
             return __communicate_plain(weight_matrix)
         elif self.method == 'choco':
             return __communicate_choco(weight_matrix)
-        elif self.method == 'dcd_psgd':
-            return __communicate_dcd_psgd(weight_matrix)
-        elif self.method == 'ecd_psgd'
-            return __communicate_ecd_psgd(weight_matrix)
 
-    def __communicate_plain(weight_matrix):
-        return None
+    def __communicate_plain(self, weight_matrix):
+        """Perform the communication step of the decentralized SGD, given the weight of
+        all the machines as a matrix, using one simple transition step of the Markov Chain"""
+        return weight_matrix @ self.transition_matrix
 
-    def __communicate_choco(weight_matrix):
-        return None
-
-    def __communicate_dcd_psgd(weight_matrix):
-        return None
-
-    def __communicate_ecd_psgd(weight_matrix):
-        return None
+    def __communicate_choco(self, weight_matrix):
+        """Perform the communication step of the decentralized SGD, given the weight of
+        all the machines as a matrix, using the Choco update"""
+        return weight_matrix + self.consensus_lr *  (weight_matrix @ (self.transition_matrix - np.eye(self.n_machines)))
