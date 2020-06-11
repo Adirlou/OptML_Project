fd91883009db9e3d07cea0274703379a2e9bad6a
adirlou <adrien.vandenbroucque@gmail.com>
2020-06-11 12:48:51 +0200

    updated JN for bottleneck + added new plotting function for mean and std

--- decentralized_SGD_least_squares.py
+++ decentralized_SGD_least_squares.py
@@ -1,17 +1,12 @@
-# This code is meant to improve existing code which comes from
-# https://github.com/epfml/ChocoSGD/tree/master/convex_code.
-# As such, much of the code below is taken directly from that repository,
-# and the credits should go to its authors.
-
 import numpy as np
 from scipy.special import expit as sigmoid
 from scipy.sparse import isspmatrix as is_sparse_matrix
 
 from decentralized_SGD_classifier import DecentralizedSGDClassifier
 
-class DecentralizedSGDLogistic(DecentralizedSGDClassifier):
+class DecentralizedSGDLeastSquares(DecentralizedSGDClassifier):
     """Class that encapsulates all attributes and methods needed to perform the decentralized SGD with a
-    logistic regression classifier"""
+    linear regression classifier"""
 
     def loss(self, A, y):
         """Compute the logistic loss given the input data and corresponding labels.
@@ -20,30 +15,13 @@ class DecentralizedSGDLogistic(DecentralizedSGDClassifier):
         """
         x_mean_machines = self.X.mean(axis=1)
 
-        # Make sure that no pred is 0 or 1 otherwise log-loss is undefined
-        pred = np.clip(self.predict_proba(A), 1e-15, 1- 1e-15)
-
         # Compute the loss
-        loss = -(y.T.dot(np.log(pred)) + (1 - y).T.dot(np.log(1 - pred))) / A.shape[0]
+        loss = np.sum((A @ x_mean_machines) - y)**2) / A.shape[0]
 
         # Add regularization
         if self.regularizer:
             loss += self.regularizer * np.sum(x_mean_machines**2) / 2
         return loss
-
-    def gradient_old(self, A, y, sample_idx, machine):
-
-        x = self.X[:, machine]
-        a = A[sample_idx]
-
-        # Compute the gradient
-        predicted = sigmoid(a.dot(x))
-        grad = (predicted - y[sample_idx]) * a
-
-        if self.regularizer:
-            grad += self.regularizer * x
-
-        return grad
 
     def gradient(self, A, y, sample_indices):
         """Compute the logistic loss gradient of the weights of each machine
@@ -67,7 +45,7 @@ class DecentralizedSGDLogistic(DecentralizedSGDClassifier):
 
         # Column-wise dot product of all machines weights with their selected random sample,
         # on which we take the sigmoid to get the prediction for each machine
-        predictions = sigmoid(np.einsum('kl,kl->l', self.X, A_rand))
+        predictions = np.einsum('kl,kl->l', self.X, A_rand)
 
         # Matrix whose columns contains the gradient corresponding
         # to each machine
@@ -84,15 +62,4 @@ class DecentralizedSGDLogistic(DecentralizedSGDClassifier):
         :param A: input data
         """
         x_mean_machines = self.X.mean(axis=1)
-        logits = A @ x_mean_machines
-        pred = 1 * (logits >= 0.)
-        return pred
-
-    def predict_proba(self, A):
-        """Predict target probabilities data of input data A using the fitted
-        model.
-        :param A: input data
-        """
-        x_mean_machines = self.X.mean(axis=1)
-        logits = A @ x_mean_machines
-        return sigmoid(logits)
+        return A @ x_mean_machines
--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from decentralized_SGD_logistic import DecentralizedSGDLogistic
 
 
 def plot_losses(losses, iterations_indices, optimum_loss=0, title="My_nice_plot",
@@ -38,6 +39,68 @@ def plot_losses(losses, iterations_indices, optimum_loss=0, title="My_nice_plot"
             plt.plot(x, y, label=labels)
         else:
             plt.plot(x, y)
+
+    plt.yscale(yscale)
+    if ylim:
+        plt.ylim(ylim)
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    if labels:
+        plt.legend(loc="best")
+
+    if save_as_pdf:
+        plt.savefig("plots/" + pdf_name + ".pdf", bbox_inches='tight')
+
+    plt.show()
+
+def plot_losses_with_std(losses, iterations_indices, optimum_loss=0, title="My_nice_plot",
+                xlabel="Iteration", ylabel="Error", labels=None, yscale="log", ylim=None,
+                figsize=(10, 5), save_as_pdf=False, pdf_name="my_nice_plot_in_PDF"):
+    """Plot the different losses with values shifted by the value of the optimal loss.
+
+    :param losses: 2D Array containing the losses for each topology
+    :param iterations_indices: 1D Array containing the indices (in number of iteration) of the given losses
+    :param optimum_loss: Value of the optimal loss
+    :param title: Title of the plot
+    :param xlabel: Label of the x axis
+    :param ylabel: Label of the y axis
+    :param labels: Labels of the different topologies, e.g., labels=["fully connected", "ring", "torus"]
+    :param yscale: Scale of the y axis, e.g., "linear", "log",...
+    :param ylim: Limits of the y axis, e.g., ylim=(0.001, 1)
+    :param figsize: Size of the plot
+    :param save_as_pdf: Saves the plot as pdf if True
+    :param pdf_name: If the plot is saved as pdf, saves with this name
+    """
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+    x = iterations_indices
+
+    plt.figure(figsize=figsize)
+
+    if len(losses) > 1:
+        # If more than one topology
+        for i in range(len(losses)):
+            mean = np.vstack(losses[i]).mean(axis=0)
+            std = np.vstack(losses[i]).std(axis=0)
+            y = mean - optimum_loss
+
+            if labels:
+                plt.plot(x, y, label=labels[i], color=colors[i])
+            else:
+                plt.plot(x, y[i], color=colors[i])
+            plt.fill_between(x, y-std, y+std, facecolor=colors[i], alpha=0.1)
+    else:
+        mean = np.vstack(losses[i]).mean(axis=0)
+        std = np.vstack(losses[i]).std(axis=0)
+        y = mean - optimum_loss
+
+        if labels:
+            plt.plot(x, y, label=labels, color=colors[0])
+        else:
+            plt.plot(x, y, color=colors[0])
+        plt.fill_between(x, y-std, y+std, facecolor=colors[0], alpha=0.1)
 
     plt.yscale(yscale)
     if ylim:
@@ -96,3 +159,20 @@ def load_data():
     A = standardize(clean(A, True))
     y = 1 *(y > 0.0)
     return y, A
+
+def run_logistic(A, y, param, logging=False):
+    m = DecentralizedSGDLogistic(**param)
+    list_losses = m.fit(A, y, logging=logging)
+    if logging:
+        print()
+        print('Final score: {0:.4f}'.format(m.score(A, y)))
+    return list_losses
+
+def run_logistic_n_times(A, y, params, n):
+    all_losses = []
+    for i in range(n):
+        print('Decentralized optimization, run number', i + 1, '\n')
+        params['data_distribution_random_seed']= i + 1
+        params['random_seed']= i + 1
+        all_losses.append(run_logistic(A, y, params, logging=True))
+    return all_losses
